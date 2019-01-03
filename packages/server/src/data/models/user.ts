a19750a9bfc82b04f2780936279efce3111e7f65
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-01-02 17:58:10 -0800

    Update User model with createdAt field

--- packages/server/src/data/models/user.ts
+++ packages/server/src/data/models/user.ts
@@ -3,6 +3,10 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
   name: String,
   picture: String,
   sub: {
