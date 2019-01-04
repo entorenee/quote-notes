18af08ff07c43c32a51c8fec75869c45faf6e9a2
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-01-03 22:46:45 -0800

    Implement CRUD on entries

--- packages/server/src/data/schema/entries.js
+++ packages/server/src/data/schema/entries.js
@@ -2,6 +2,7 @@ import { gql } from 'apollo-server-express';
 import mongoose from 'mongoose';
 
 const Entry = mongoose.model('Entry');
+const User = mongoose.model('User');
 
 export const typeDefs = gql`
   type Entry {
@@ -15,6 +16,22 @@ export const typeDefs = gql`
     quote: String
   }
 
+  input NewEntryInput {
+    book: ID!
+    chapter: String
+    notes: String
+    page: Int
+    quote: String
+  }
+
+  input UpdateEntryInput {
+    id: ID!
+    chapter: String
+    notes: String
+    page: Int
+    quote: String
+  }
+
   extend type User {
     entries: [Entry]
   }
@@ -22,11 +39,70 @@ export const typeDefs = gql`
   extend type Query {
     entry(id: ID!): Entry
   }
+
+  extend type Mutation {
+    createEntry(input: NewEntryInput): Entry
+    updateEntry(input: UpdateEntryInput): Entry
+    removeEntry(id: ID!): ID
+  }
 `;
 
 export const resolvers = {
   Query: {
     entry: (_, { id }) => Entry.findById(id),
+  },
+  Mutation: {
+    createEntry: async (_, { input }, { user }) => {
+      if (!user) return null;
+
+      const userObj = await User.findOne({ sub: user.sub });
+      if (!userObj) return null;
+
+      const { id: owner } = userObj;
+      const data = {
+        ...input,
+        owner,
+      };
+
+      const entry = new Entry(data);
+      const newEntry = await entry.save();
+      const { id: entryId } = newEntry;
+      await User.findByIdAndUpdate(
+        owner,
+        { $push: { entries: entryId } },
+        { new: true },
+      );
+
+      return newEntry;
+    },
+    updateEntry: async (_, { input }, { user }) => {
+      if (!user) return null;
+
+      const userObj = await User.findOne({ sub: user.sub });
+      if (!userObj) return null;
+
+      const { id: owner } = userObj;
+      return Entry.findOneAndUpdate({ _id: input.id, owner }, input, {
+        new: true,
+      });
+    },
+    removeEntry: async (_, { id }, { user }) => {
+      if (!user) return null;
+
+      const userObj = await User.findOne({ sub: user.sub });
+      if (!userObj) return null;
+
+      const { id: owner } = userObj;
+
+      await User.findByIdAndUpdate(
+        owner,
+        { $pull: { entries: id } },
+        { new: true },
+      );
+
+      const removed = await Entry.findOneAndRemove({ _id: id, owner });
+      return removed.id;
+    },
   },
   User: {
     entries: ({ entries }) =>
