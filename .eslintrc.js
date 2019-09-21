c3bd524427bcf0e90ce90257d56b466de6434489
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-09-21 09:44:55 -0700

    chore: update eslint and move to my config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,3 @@
 module.exports = {
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  env: {
-    'jest/globals': true
-  },
-  plugins: ['react', 'jest', 'prettier'],
-  extends: ['airbnb', 'prettier', 'prettier/react'],
-  rules: {
-    'react/jsx-filename-extension': 0,
-  }
+  extends: ['dslemay', 'dslemay/react', 'dslemay/jest'],
 };
