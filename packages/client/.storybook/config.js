2510ad8c8f3f781bba30845e9e79c0b0a4be022c
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-11-14 20:31:49 -0800

    feat: install Storybook backgrounds package

--- packages/client/.storybook/config.js
+++ packages/client/.storybook/config.js
@@ -1,7 +1,14 @@
-import { addDecorator, configure } from '@storybook/react'
+import { addDecorator, addParameters, configure } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
 
 addDecorator(withKnobs)
+
+addParameters({
+  backgrounds: [
+    { name: 'Light', value: '#F5F7F3', default: true },
+    { name: 'Dark', value: '#1E141F' }
+  ]
+})
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /bs.js$/), module)
