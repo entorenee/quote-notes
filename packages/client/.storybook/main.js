81c4d413e4d2b2be09df0c72f15f633d02f46416
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2020-02-07 15:01:39 -0800

    feat: add a11 Storybook plugin

--- packages/client/.storybook/main.js
+++ packages/client/.storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../stories/**/*.bs.js'],
   addons: [
     '@storybook/addon-actions',
+    '@storybook/addon-a11y',
     '@storybook/addon-links',
     '@storybook/addon-backgrounds',
   ],
