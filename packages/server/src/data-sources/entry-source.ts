eeda917808108772f4ed33139f3296c54593b4a6
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2020-04-30 16:43:05 -0700

    feat: add entry CRUD functionality

--- packages/server/src/data-sources/entry-source.ts
+++ packages/server/src/data-sources/entry-source.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-cycle */
 import DataLoader from 'dataloader';
+import { QueryBuilder } from 'knex';
 
+import { NexusGenInputs } from '../generated/nexus-types.gen';
 import { EntriesEntity } from '../generated/db-types';
 import { byColumnLoader, manyByColumnLoader } from '../utils/loader-utils';
 import Context from './context';
@@ -11,6 +13,23 @@ class EntrySource {
   public constructor(ctx: Context) {
     this.ctx = ctx;
   }
+
+  private entryReturn: Extract<keyof EntriesEntity, string>[] = [
+    'id',
+    'userBookId',
+    'userId',
+    'createdAt',
+    'updatedAt',
+    'title',
+    'notes',
+    'chapter',
+    'pages',
+    'quote',
+  ];
+
+  private writeEntry: QueryBuilder = this.ctx
+    .knex<EntriesEntity>('entries')
+    .returning(this.entryReturn);
 
   private byIdLoader = new DataLoader<string, EntriesEntity>(ids => {
     return byColumnLoader(this.ctx, 'entries', 'id', ids);
@@ -34,6 +53,52 @@ class EntrySource {
 
   public byUserId(id: string): Promise<EntriesEntity[]> {
     return this.byUserIdLoader.load(id);
+  }
+
+  public async currentUserEntries(): Promise<EntriesEntity[] | null> {
+    const userId = await this.ctx.user.userId;
+    return userId ? this.byUserId(userId) : null;
+  }
+
+  public async createEntry(
+    data: NexusGenInputs['NewEntryInput'],
+  ): Promise<EntriesEntity> {
+    const userId = await this.ctx.user.userId;
+    if (!userId) {
+      return Promise.reject(
+        new Error('You must be authenticated to create an entry'),
+      );
+    }
+    const input = {
+      ...data,
+      userId,
+    };
+    return this.writeEntry.insert(input);
+  }
+
+  public async updateEntry(
+    data: NexusGenInputs['UpdateEntryInput'],
+  ): Promise<EntriesEntity> {
+    const userId = await this.ctx.user.userId;
+    if (!userId) {
+      return Promise.reject(
+        new Error('You must be authenticated to update an entry'),
+      );
+    }
+    return this.writeEntry.where({ userId, id: data.id }).update(data);
+  }
+
+  public async deleteEntry(id: string): Promise<Pick<EntriesEntity, 'id'>> {
+    const userId = await this.ctx.user.userId;
+    if (!userId) {
+      return Promise.reject(
+        new Error('You must be authenticated to delete an entry'),
+      );
+    }
+    return this.writeEntry
+      .where({ userId, id })
+      .del(id)
+      .returning(['id']);
   }
 }
 
